a7696c7c0b84441ef06a0ffb5167463133a0539e
Given16 <manaleng99@gmail.com>
2023-12-18 17:43:49 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
 /**
  * main - entry point
  * Program that prints the lowercase alphabet in reversei.
  * Return: (0)
  */
-
 int main(void)
 {
 	char alph;
-	
-	for (alph = 'z'; alp >= 'a'; alph--)
+
+	for (alph = 'z'; alph >= 'a'; alph--)
 	{
 		putchar(alph);
 	}
